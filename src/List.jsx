54d20fb3fcfc96a26561c86a8267be881ef0cd22
archishmagoli <agoli6@gatech.edu>
2024-03-30 15:16:13 -0400

    finish search functionality

--- src/List.jsx
+++ src/List.jsx
@@ -1,21 +1,56 @@
 import axios from 'axios';
 import { useState, useEffect } from 'react';
+import Select from 'react-dropdown-select';
+import './App.css'
 
 const List = () => {
     const URL = 'https://api.openbrewerydb.org/v1/breweries';
-    const [breweries, setBreweries] = useState(null);
+    const [breweries, setBreweries] = useState([]);
+    const [typeFilter, setTypeFilter] = useState(null);
+    const [search, setSearch] = useState(null);
+
+    const brewery_types = [
+        {value: 'large', label: 'large'},
+        {value: 'micro', label: 'micro'},
+        {value: 'nano', label: 'nano'},
+        {value: 'regional', label: 'regional'},
+        {value: 'brewpub', label: 'brewpub'},
+        {value: 'planning', label: 'planning'},
+        {value: 'bar', label: 'bar'},
+        {value: 'contract', label: 'contract'},
+        {value: 'proprietor', label: 'proprietor'},
+        {value: 'closed', label: 'closed'}
+    ]
 
     useEffect(() => {
         const fetchData = async () => {
-            const response = await axios.get(URL);
+            let response;
+            let MOD_URL = URL;
+
+            if (typeFilter !== null) {
+                MOD_URL = MOD_URL + '?by_type=' + typeFilter;
+            }
+            
+            if (search !== null) {
+                
+                MOD_URL = MOD_URL + (typeFilter === null ? '?' : '&') + 'by_name=' + encodeURIComponent(search);
+            }
+
+            response = await axios.get(MOD_URL);
+
+            console.log(MOD_URL);
+
             setBreweries(response.data);
         }
         fetchData();
-    }, []);
-
+    }, [typeFilter, search]);
 
     return (
         <>
+        <label htmlFor='nameSearch'>Search By Name</label>
+        <input id='nameSearch' name="nameSearch" type="text" onChange={e => setSearch(e.target.value)} />
+
+        <Select options={brewery_types} className='brewery_dropdown' onChange={(type) => setTypeFilter(type[0].value)} />
         <table>
             <thead>
                 <tr>
@@ -32,7 +67,7 @@ const List = () => {
                     breweries.map(brewery => 
                         <tr key={brewery.id}>
                             <td>{brewery.name}</td>
-                            <td>{brewery.type}</td>
+                            <td>{brewery.brewery_type}</td>
                             <td>{brewery.address_1}, {brewery.city}, {brewery.state_province}</td>
                             <td>{brewery.country}</td>
                             <td>{brewery.website_url ? brewery.website_url : 'None'}</td>
